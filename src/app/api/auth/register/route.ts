16410fd18ebd326cc1317c4e7b2a5a344d939711
وجيه جبر <wajeeh.jaber20@gmail.com>
2026-02-20 22:08:41 +0200

    fix bcrypt import

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
+import * as bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 
 export async function POST(req: Request) {
